db10fbd251fb19ddb30f87bcd0c3d7c5e9be2da8
Alessio Ghidini <97110278+Lobsterge@users.noreply.github.com>
2023-08-07 21:52:02 +0200

    FIrst commit

---
+++ client.py
@@ -0,0 +1,103 @@
+import socket
+from _thread import *
+import tkinter as tk
+import tkinter.scrolledtext as st
+import webbrowser
+from tkinter.messagebox import showinfo
+
+check=False 
+
+def startup():
+    global s
+    global check
+    if check==True:
+        showinfo("Errore", "Sei già connesso")
+        return
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host="localhost"
+    port=4444
+    try:
+        s.connect((host,port))
+    except:
+        showinfo("Errore", "Non è stato possibile effettuare una connessione")
+        return
+    status.config(text="Connesso", fg="green")
+    check=True
+    start_new_thread(server_handler, (s, False))
+
+
+def server_handler(connection, startup):
+    try:
+        while True:
+            data = connection.recv(1024)
+            if data:
+                messaggio=data.decode()
+                output.configure(state ='normal')
+                fully_scrolled_down = output.yview()[1] == 1.0
+                output.insert("end", messaggio)
+                if fully_scrolled_down:
+                    output.see("end")
+                output.configure(state ='disabled')
+            else:
+                break    
+    finally:
+        connection.close()
+        status.config(text="Non connesso", fg="red")
+        output.configure(state ='normal')
+        output.insert("end", ">Il server è stato spento, vai a picchiare chiunque lo stava hostando\n")
+        global check
+        check=False
+        output.configure(state ='disabled')
+
+def send_data(self=None):
+    global check
+    if check==False:
+        showinfo("Errore", "Non sei connesso")
+        return
+    message=input.get("1.0", tk.END).rstrip().replace("\n", " ")
+    input.delete("1.0", tk.END)
+    try:
+        if message.rstrip()!="":
+            s.send((message+"\n").encode())
+    except:
+        showinfo("Errore", "Non mandare cose strane plz")
+
+'''UI'''  
+
+def open_link():
+    webbrowser.open_new_tab("https://github.com/")    
+
+def scroll_up(self=None):
+    output.yview_scroll(-1, "units")
+
+def scroll_down(self=None):
+    output.yview_scroll(1, "units")
+
+win = tk.Tk()
+win.title("Chat")
+
+output = st.ScrolledText(win,width = 75, height = 16)
+help = tk.Button(text="Aiuto", width=10, height=1, command=open_link)
+padding= tk.Frame()
+status = tk.Label(master=padding, text="Non connesso", fg="red", height=1)
+input = tk.Text(height=3)
+send = tk.Button(text="Invia", width=10, height=1, command=send_data)
+connect = tk.Button(master=padding, text="Connettiti", width=10, height=1, command=startup)
+
+
+help.pack(anchor=tk.NW)  
+output.pack(fill=tk.BOTH, expand=True)
+connect.pack(side=tk.LEFT, anchor=tk.W)
+status.pack(side=tk.LEFT, anchor=tk.W)
+padding.pack(anchor=tk.W)
+input.pack(fill=tk.X)
+send.pack(anchor=tk.SE)  
+
+
+output.configure(state ='disabled')
+win.bind("<Return>", send_data)
+win.bind("<KP_Up>", scroll_up)
+win.bind("<KP_Down>",scroll_down)
+win.bind("<Up>", scroll_up)
+win.bind("<Down>",scroll_down)
+win.mainloop()  
---
+++ server.py
@@ -0,0 +1,118 @@
+import socket 
+from _thread import *
+from datetime import datetime
+
+#per killare il processo in caso di errori: fuser -k 4444/tcp
+
+logging=False # stampa chatlog su file
+debug=False # stampa chatlog su terminale
+
+if logging:
+    path="log_"+str(datetime.now())[:-7]+".txt"
+    log=open("log_"+str(datetime.now())[:-7]+".txt", "w")
+    log.close()
+    
+logged_usernames=[]
+users={}
+
+def client_handler(connection, address):
+    welcome_message=">Seleziona un username per iniziare a comunicare (massimo 20 caratteri):\n"
+    connection.sendall(welcome_message.encode())
+    username=connection.recv(1024).rstrip().decode()
+
+    if len(username)>20:
+        username=username[:20]
+
+    while username in logged_usernames:
+        connection.sendall("Username già presente nella stanza, riprovare:\n".encode())
+        username=connection.recv(20).rstrip().decode()
+
+    time="["+datetime.now().strftime("%H:%M")+"]"
+    notify=time+"--> "+username+" è entrato nella stanza\n"
+
+    if debug: 
+        print(notify.rstrip(), address, sep=" ")
+    if logging:
+        log=open(path, "a")
+        print(notify.rstrip(), address, sep=" ", file=log)
+        log.close()
+
+    for user in logged_usernames:
+        users[user].sendall(notify.encode())
+
+    logged_usernames.append(username)
+    connection.sendall(b"Ok!\n")
+    users[username]=connection
+    
+    try:
+        while True:
+            data = connection.recv(1024)
+            if data:
+                time="["+datetime.now().strftime("%H:%M")+"]"
+                messaggio=time+username+": "+data.decode()
+
+                if debug:
+                    print(messaggio.rstrip(), address)  
+
+                if logging:
+                    log=open(path, "a")
+                    print(messaggio.rstrip(), address, file=log) 
+                    log.close() 
+
+                for user in logged_usernames:
+                    users[user].sendall(messaggio.encode())
+            else:
+                break    
+    finally:
+        time="["+datetime.now().strftime("%H:%M")+"]"
+        notify=time+"<-- "+username+" è uscito dalla stanza\n"
+
+        if debug: 
+            print(notify.rstrip(), address)
+        if logging:
+            log=open(path, "a")
+            print(notify.rstrip(), address, file=log)
+            log.close()
+
+        logged_usernames.remove(username)
+
+        for user in logged_usernames:
+            users[user].sendall(notify.encode())
+        
+        print(time+' Disconnected from:', address[1:-1])
+
+        if logging:
+            log=open(path, "a")
+            print(time+' Disconnected from:', address[1:-1], file=log)
+            log.close()
+
+        connection.close()
+        users.pop(username)
+
+def accept_connection(socket):
+    client, address = socket.accept()
+
+    address="("+address[0]+":"+str(address[1])+")"
+    time="["+datetime.now().strftime("%H:%M")+"]"
+
+    print(time+" Connected to: "+address[1:-1])
+
+    if logging:
+        log=open(path, "a")
+        print(time+" Connected to: "+address[1:-1], file=log)
+        log.close()
+
+    start_new_thread(client_handler, (client, address)) 
+
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host="localhost"
+port=4444    
+
+socket.bind((host,port))
+socket.listen(30)
+
+
+while True:
+   accept_connection(socket)
